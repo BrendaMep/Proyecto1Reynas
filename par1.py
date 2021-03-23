c78b13b4c8e0044a4263d2bcdb9d1022bc97ad55
Home <BrendaMep>
2021-03-23 15:35:48 -0600

    Comienzo de la funcion de generar la poblacion.

--- par1.py
+++ par1.py
@@ -4,13 +4,15 @@ import random
 # Funcion de generacion de poblacion.
 
 def poblacion(z):
-    reynas = np.zeros((8, 8),z)
+    reynas = np.zeros([0, 8, z])
     for n in range(0, z):
         for m in range(0,8):
-            posicion = random.randrange(0,8)
+            posicion = random.randrange(8)
             reynas[0, m, n] = posicion
-    print(reynas)
+            print(reynas)
     return reynas
 
 poblacion(8)
+reynas = np.zeros([0, 8, 1])
+print(reynas)
 
