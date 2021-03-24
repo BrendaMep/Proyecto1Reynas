7b0b8902a7cb43d58d45aa21d6d598b8ff697c1c
Home <BrendaMep>
2021-03-23 18:42:30 -0600

    solo cambio un poco la redaccion

--- par1.py
+++ par1.py
@@ -3,14 +3,15 @@ import random
 
 # Funcion de generacion de poblacion.
 
-def posicion(x, y, z):   # marca en cual fila se encuentra una reyna
+def posicion(x, y, z):   # marca en cual columna se encuentra una reyna
     reyna = np.zeros([x, y, z])
     for n in range(0, z):
         for i in range(y):
-            columna = random.randrange(y)   #nos dice en cual columna se encuentra
-            reyna[x-1, i, n] = columna
+            columna = random.randrange(y)   #nos da una columna aleatoria en la que encontrara la reina
+            reyna[x-1, i, n] = columna      # i es la fila y columna la posicion de la reina
     return reyna
 
 posicion(1, 8, 50)
 poblacion = posicion(1,8,50)
 
+#Funcion seleccion de padres metodo Sobrante estocastico
