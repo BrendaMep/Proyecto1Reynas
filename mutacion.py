7de2a4bf54bce525a65c562f7dc289a6304a06e6
Home <BrendaMep>
2021-04-06 18:20:37 -0500

    solo funciona en algunos casos :(

--- mutacion.py
+++ mutacion.py
@@ -8,29 +8,19 @@ def mut_insercion(hijo):
     matrix_zeros = np.zeros((1,8))
     posicion1 = random.randrange(7) # nos dara el numero de una columna al azar.
     posicion2 = random.randrange(8)
-
-    if posicion2 == posicion1:
-        posicion2 = random.randrange(8)
-
-    minimo = min(posicion1, posicion2)
-    maximo = max(posicion1, posicion2)
-    if minimo == posicion2:
-        for i in range(0, minimo):
+    print(posicion1)
+    print(posicion2)
+    if posicion1 < posicion2:
+        for i in range(0, posicion1 + 1):
             matrix_zeros[0, i] = mutado[i]
-        print(matrix_zeros[:,:])
-        for i in range(minimo+1, posicion1):
+        for i in range(posicion1 + 1, posicion1 + 2):
+            matrix_zeros[0, i] = mutado[posicion2]
+        for i in range(posicion1 + 1, posicion2):
+            matrix_zeros[0, posicion1 - 1 + i] = mutado[i]
+        for i in range(posicion2 + 1, 8):
             matrix_zeros[0, i] = mutado[i]
-
-    else:
-        for i in range(0, minimo+1):
-            matrix_zeros[0, i] = mutado[i]
-        if i == posicion2 :
-            matrix_zeros[0,minimo+1] = mutado[posicion2]
-        for i in range(minimo+1,posicion2):
-            matrix_zeros[0,i] = mutado[posicion1+i]
-        for i in range(posicion2,8):
-            matrix_zeros[0, i ] = mutado[i]
-        print(matrix_zeros[:, :])
+        hijo_mut = matrix_zeros
+        print(hijo_mut)
 
 
 m = np.array([0,1,2,3,4,5,6,7])
