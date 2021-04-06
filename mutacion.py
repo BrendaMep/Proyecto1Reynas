7e97d5fb4ff692ba7889aae96f261a2a065c3a12
Home <BrendaMep>
2021-04-06 17:40:51 -0500

    se tiene errores aun.

--- mutacion.py
+++ mutacion.py
@@ -14,9 +14,24 @@ def mut_insercion(hijo):
 
     minimo = min(posicion1, posicion2)
     maximo = max(posicion1, posicion2)
-    for i in range(0, minimo):
-        matrix_zeros[0, i] = mutado[i]
-    print(matrix_zeros[:,:])
-    for i in range(minimo, maximo)
+    if minimo == posicion2:
+        for i in range(0, minimo):
+            matrix_zeros[0, i] = mutado[i]
+        print(matrix_zeros[:,:])
+        for i in range(minimo+1, posicion1):
+            matrix_zeros[0, i] = mutado[i]
+
+    else:
+        for i in range(0, minimo+1):
+            matrix_zeros[0, i] = mutado[i]
+        if i == posicion2 :
+            matrix_zeros[0,minimo+1] = mutado[posicion2]
+        for i in range(minimo+1,posicion2):
+            matrix_zeros[0,i] = mutado[posicion1+i]
+        for i in range(posicion2,8):
+            matrix_zeros[0, i ] = mutado[i]
+        print(matrix_zeros[:, :])
+
+
 m = np.array([0,1,2,3,4,5,6,7])
 mut_insercion(m)
