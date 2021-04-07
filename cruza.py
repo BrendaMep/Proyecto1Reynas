71a7d4237d33ca33df32b5e6d1510aeb64489bb6
Home <BrendaMep>
2021-04-07 11:17:09 -0500

    se esta mas cerca del exito, pero salen mas de lo que debia.

--- cruza.py
+++ cruza.py
@@ -6,10 +6,19 @@ ale_inicial = random.randrange(8)
 def cruza_ext(p1,p2):
     for i in range(8):
         if i in range(7):
-            elemen = np.array([p1[i - 1], p1[i + 1], p2[p1[i] - 1], p2[p1[i] + 1]])
-            print(elemen)
+            for j in range(8):
+                if j in range(7):
+                    if p1[i] == p2[j]:
+                        elemen = np.array([p1[i - 1], p1[i + 1], p2[p1[j] - 1], p2[p1[j] + 1]])
+                        print(elemen)
+                else:
+                    elemen = np.array([p1[i - 1], p1[i + 1], p2[6], p2[0]])
+                    print(elemen)
         else:
-            elemen = np.array([p1[6], p1[0], p2[0], p2[0]])
+            for j in range(8):
+                if j in range(7):
+                    if p1[i] == p2[j]:
+                        elemen = np.array([p1[6], p1[0], p2[p1[j] - 1], p2[p1[j] + 1]])
             print(elemen)
 
 
