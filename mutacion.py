fa152f6df039dea1182916865bf11e2f2ac1ec6a
Home <BrendaMep>
2021-04-06 20:45:04 -0500

    Mutacion completada por el metodo de insercion

--- mutacion.py
+++ mutacion.py
@@ -10,34 +10,43 @@ def mut_insercion(hijo):
     posicion2 = random.randrange(8)
     print(posicion1)
     print(posicion2)
+# aqui comienza la mutacion.
 
     if posicion1 < posicion2:
-        for i in range(0, posicion1 + 1):
+        for i in range(0, posicion1 + 1):  # los lugares que son <= posicion1 permanecen es su lugar
             matrix_zeros[0, i] = mutado[i]
-        for i in range(posicion1 + 1, posicion1 + 2):
+
+        for i in range(posicion1 + 1, posicion1 + 2): # el concecutivo de posicion 1 es posicion2
             matrix_zeros[0, i] = mutado[posicion2]
-        for i in range(posicion1 + 1, posicion2):
+
+        for i in range(posicion1 + 1, posicion2): # los x entre las 2 posiciones
             matrix_zeros[0, i + 1] = mutado[i]
-        for i in range(posicion2 + 1, 8):
+
+        for i in range(posicion2 + 1, 8): # los que estan despues de posicion2 permanecen en su lugar
             matrix_zeros[0, i] = mutado[i]
-        hijo_mut = matrix_zeros
-        print(hijo_mut)
+        mutado = matrix_zeros
+        return mutado
     else:
         for i in range(0, posicion2):
             matrix_zeros[0, i] = mutado[i]
+
         for i in range(posicion1, posicion1 + 1):
             matrix_zeros[0, i] = mutado[i]
+
         for i in range(posicion1 + 1, 8):
             matrix_zeros[0, i] = mutado[i]
+
         for i in range(posicion1 + 1, posicion1 + 2):
             matrix_zeros[0, i] = mutado[posicion2]
+
         for i in range(posicion2 + 1, posicion1):
             matrix_zeros[0, i] = mutado[i]
+
         for i in range(posicion1 + 1, posicion1 + 2):
             matrix_zeros[0, posicion2] = mutado[i]
-        hijo_mut = matrix_zeros
-        print(hijo_mut)
+        mutado = matrix_zeros
+        return mutado
 
 
 m = np.array([0,1,2,3,4,5,6,7])
-mut_insercion(m)
+print(mut_insercion(m))
