c75ddf4370d1e7096eabf62829eb918471183811
Home <BrendaMep>
2021-04-09 18:30:16 -0500

    Funciona el reemplazamiento, lo que hace es tomar el mejor mutado de los hijos y ese lo sustituye aleatoriamente en la poblacion inicial.

--- reemplazamiento.py
+++ reemplazamiento.py
@@ -1,8 +1,15 @@
 import numpy as np
 import random
 # reemplazamiento elitismo
+lista = [0, 1, 2, 3, 4, 5, 6, 7] # son los valores en los que puede estar la reyna
+poblacion = np.empty((50,8))
+for i in range(50):
+    random.shuffle(lista)
+    for j in range(8):
+        poblacion[i, j] = lista[j]
+print(poblacion)
+
 def reemplazamiento(hijo1, hijo2):
-    poblacion = np.empty((50, 8))
     rein=np.array([6,2,7,1,4,0,5,3]) # es la reina con cero ataques
     count1 = 0
     count2 = 0
@@ -13,3 +20,16 @@ def reemplazamiento(hijo1, hijo2):
             count2 +=1
 
     maximo = max(count1,count2)
+    if maximo == count1:
+        cambio = random.randrange(50)
+        print(cambio)
+        poblacion[cambio,:]= hijo1
+    else:
+        cambio = random.randrange(50)
+        poblacion[cambio,:] = hijo2
+        print(cambio)
+
+hijo1= [0,1,2,3,4,5,6,7]
+hijo2= [6,2,7,1,3,5,4,0]
+reemplazamiento(hijo1,hijo2)
+print(poblacion)
