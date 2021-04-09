7c6902b4044f7bab635eb0662f8225b3c5d2cf72
Home <BrendaMep>
2021-04-09 11:20:03 -0500

    listo poblacion completado.

--- par1.py
+++ par1.py
@@ -2,16 +2,14 @@ import numpy as np
 import random
 
 # Funcion de generacion de poblacion.
+lista = [0, 1, 2, 3, 4, 5, 6, 7] # son los valores en los que puede estar la reyna
+poblacion = np.empty((50,8))
+for i in range(50):
+    random.shuffle(lista)
+    for j in range(8):
+        poblacion[i, j] = lista[j]
 
-def posicion(x, y, z):   # marca en cual columna se encuentra una reyna
-    reyna = np.zeros((x, y, z))
-    for n in range(0, z):
-        for i in range(y):
-            columna = random.choice(range(y))   #nos da una columna aleatoria en la que encontrara la reina
-            reyna[x-1, i, n] = columna  # i es la fila y columna la posicion de la reina
-    return reyna
 
-posicion(1, 8, 50)
-poblacion = posicion(1,8,50)
-print(poblacion[:,:,34])
-#Funcion seleccion de padres metodo Sobrante estocastico
+hijo1 = poblacion[36,:]
+hijo2 = poblacion[12,:]
+
