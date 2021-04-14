c3d9d2c71225647a466b3b311f7d205edcac3f81
Home <BrendaMep>
2021-04-13 20:17:57 -0500

    nos da los numeros que se encuentran en los extremos, se escoge le numero current element y se elimina de toda la tabla de extremos

--- cruza.py
+++ cruza.py
@@ -3,24 +3,42 @@ import random
 import numpy as np
 
 def cruza_ext(padre1,padre2):
-    elemen = [1, 2, 3, 4, 5, 6, 7, 8]
+    tabla = [[0],[1],[2],[3],[4],[5],[6],[7]]
     for i in range(8):
         if i in range(7):
             for j in range(8):
                 if j in range(7) and padre1[i] == padre2[j]:
-                    elemen[i] = [padre1[i - 1], padre1[i + 1], padre2[j - 1], padre2[j + 1]]
-                    print(elemen)
+                    tabla[i] = [padre1[i - 1], padre1[i + 1], padre2[j - 1], padre2[j + 1]]
+
                 if j == 7 and padre1[i] == padre2[j]:
-                    elemen[i] = [padre1[i - 1], padre1[i + 1], padre2[j - 1], padre2[0]]
-                    print(elemen)
+                    tabla[i] = [padre1[i - 1], padre1[i + 1], padre2[j - 1], padre2[0]]
         else:
             for j in range(8):
                 if j in range(7) and padre1[i] == padre2[j]:
-                    elemen[i] = [padre1[6], padre1[0], padre2[j - 1], padre2[j + 1]]
-                    print(elemen)
+                    tabla[i] = [padre1[6], padre1[0], padre2[j - 1], padre2[j + 1]]
+
                 if padre1[7] == padre2[7]:
-                    elemen[i] = [padre1[6], padre1[0], padre2[6], padre2[0]]
-                    print(elemen)
+                    tabla[i] = [padre1[6], padre1[0], padre2[6], padre2[0]]
+
+    tabla_extremos = tabla
+    print(tabla_extremos)
+    current_element = random.randrange(8)
+    print(current_element)
+    cruza = []
+    remover = [[0],[1],[2],[3],[4],[5],[6],[7]]
+    for i in range(8):
+        remover[i] = list(tabla_extremos[i])
+        if padre1[i] == current_element:
+            print(tabla_extremos[i])
+            cruza.append(current_element)
+            print(cruza)
+        if current_element in remover[i]:
+            remover[i].remove(current_element)
+            if current_element in remover[i]:
+                remover[i].remove(current_element)
+                print(remover[i])
+            else:
+                print(remover[i])
 
 
 p1 = np.array([7,1,2,3,4,5,6,0])
