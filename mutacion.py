d89cd47ba6abc06d8e0af71b27eff43aafa199f0
Home <BrendaMep>
2021-04-28 08:21:34 -0500

    Se hicieron varioos cambios ya que al estar uniedo todo se tenia algunos problemas, pero ya se resolvio

--- mutacion.py
+++ mutacion.py
@@ -7,30 +7,27 @@ def mut_insercion(hijo):
     matrix_zeros = ([])
     posicion1 = random.randrange(7) # nos dara el numero de una columna al azar.
     posicion2 = random.randrange(8)
+    print(posicion1,posicion2)
             # aqui comienza la mutacion.
     if posicion1 < posicion2:
-        for i in range(0, posicion1 + 1):  # los lugares que son <= posicion1 permanecen es su lugar
-            matrix_zeros.append( mutado[i])
-        for i in range(posicion1 + 1, posicion1 + 2): # el concecutivo de posicion 1 es posicion2
-            matrix_zeros.append( mutado[posicion2])
-        for i in range(posicion1 + 1, posicion2): # los x entre las 2 posiciones
-            matrix_zeros.append( mutado[i])
-        for i in range(posicion2 + 1, 8): # los que estan despues de posicion2 permanecen en su lugar
+        for i in range(0,posicion1+1):
             matrix_zeros.append(mutado[i])
-        mutado = matrix_zeros
-        return mutado
-    else:
-        for i in range(0, posicion2):
+        for i in range(posicion2, posicion2+1):
             matrix_zeros.append(mutado[i])
-        for i in range(posicion1, posicion1 + 1):
-            matrix_zeros.append( mutado[i])
-        for i in range(posicion1 + 1, 8):
-            matrix_zeros.append( mutado[i])
-        for i in range(posicion1 + 1, posicion1 + 2):
-            matrix_zeros.append(mutado[posicion2])
-        for i in range(posicion2 + 1, posicion1):
+        for i in range(posicion1+1,posicion2):
+            matrix_zeros.append((mutado[i]))
+        for i in range(posicion2+1,8):
             matrix_zeros.append(mutado[i])
-        for i in range(posicion1 + 1, posicion1 + 2):
-            matrix_zeros.append( mutado[i])
-        mutado = matrix_zeros
+    if posicion2 < posicion1:
+        for i in range(0,posicion2):
+            matrix_zeros.append(mutado[i])
+        for i in range(posicion1,posicion1+1):
+            matrix_zeros.append(mutado[i])
+        for i in range(posicion2,posicion2+1):
+            matrix_zeros.append(mutado[i])
+        for i in range(posicion2+1,posicion1):
+            matrix_zeros.append(mutado[i])
+        for i  in range(posicion1+1, 8):
+            matrix_zeros.append(mutado[i])
+    mutado = matrix_zeros
     return mutado
